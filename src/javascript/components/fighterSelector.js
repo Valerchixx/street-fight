5b3f492eb86ca0a0e6f8f86826512143e80e5973
Valeria Proshachenko <vproshachenko@gmail.com>
2023-12-14 10:32:57 +0200

    implement getFighterInfo function

--- src/javascript/components/fighterSelector.js
+++ src/javascript/components/fighterSelector.js
@@ -1,12 +1,17 @@
 import createElement from '../helpers/domHelper';
 import renderArena from './arena';
+import fighterService from '../services/fightersService';
 import versusImg from '../../../resources/versus.png';
 import { createFighterPreview } from './fighterPreview';
 
 const fighterDetailsMap = new Map();
 
 export async function getFighterInfo(fighterId) {
-    // get fighter info from fighterDetailsMap or from service and write it to fighterDetailsMap
+    if (!fighterDetailsMap.has(fighterId)) {
+        fighterDetailsMap.set(fighterId, await fighterService.getFighterDetails(fighterId));
+    }
+
+    return fighterDetailsMap.get(fighterId);
 }
 
 function startFight(selectedFighters) {
